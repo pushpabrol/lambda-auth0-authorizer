0ef98155792114d4a64b6daffe64f2d8c357521d
Bradley Plies <brad.plies@vestwell.com>
2021-10-19 15:14:20 -0500

    RED-158 Adjusted validation conditions for Lambda Event Payload format 2.0.  Updated JSON sample

--- lib.js
+++ lib.js
@@ -18,14 +18,14 @@ const getPolicyDocument = (effect, resource) => {
 
 
 // extract and return the Bearer Token from the Lambda event parameters
-const getToken = (params) => {
-    if (!params.type || params.type !== 'TOKEN') {
-        throw new Error('Expected "event.type" parameter to have value "TOKEN"');
+const getToken = (payload) => {
+    if (!payload.type || payload.type !== 'REQUEST') {
+        throw new Error('Expected "event.type" parameter to have value "REQUEST"');
     }
 
-    const tokenString = params.authorizationToken;
+    const tokenString = payload.headers.authorization;
     if (!tokenString) {
-        throw new Error('Expected "event.authorizationToken" parameter to be set');
+        throw new Error('Expected "event.headers.authorization" parameter to be set');
     }
 
     const match = tokenString.match(/^Bearer (.*)$/);
