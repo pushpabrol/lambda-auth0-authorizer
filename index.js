f9824f85b0fc9481fdf768a9c8adeebd6e584be2
Soren Jensen <soren@invertbio.com>
2023-10-19 14:45:51 +0100

    First step in adding sentry

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
 const lib = require('./lib');
+const Sentry = require("@sentry/serverless");
+
+Sentry.AWSLambda.init({
+  dsn: process.env.SENTRY_DSN,
+  tracesSampleRate: 1.0,
+});
+
 let data;
 
-// Lambda function index.handler - thin wrapper around lib.authenticate
-module.exports.handler = async (event, context, callback) => {
+module.exports.handler = Sentry.AWSLambda.wrapHandler(async (event, context) => {
   try {
     data = await lib.authenticate(event);
   }
   catch (err) {
-      console.log(err);
+      console.error(err);
+      Sentry.captureException(err);
       return context.fail("Unauthorized");
   }
   return data;
-};
+});
