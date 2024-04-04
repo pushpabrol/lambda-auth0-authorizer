36c3f884e6d2d4eaa5b124f7a8d79ec8541bd607
Kristian Dupont <kristian@kristiandupont.com>
2024-04-04 10:22:00 +0200

    Fix newlines in env var

--- lib.js
+++ lib.js
@@ -62,7 +62,9 @@ module.exports.authenticate = (params) => {
     );
   } else {
     // Otherwise, this is a JWT we've issued ourselves, so we can use the public key we have
-    signingKeyPromise = Promise.resolve(process.env.PUBLIC_KEY);
+    signingKeyPromise = Promise.resolve(
+      process.env.PUBLIC_KEY.replace(/\\n/g, "\n")
+    );
   }
 
   return signingKeyPromise
